00121c4e2af1bbbde11a2b2ea3a3a82b576dcfd9
OLEWSKI Clement <clement.olewski@ipilyon.net>
2020-02-13 18:31:43 +0100

    Ajout mapper

--- backend/src/main/java/com/ipilyon/backend/model/Test.java
+++ backend/src/main/java/com/ipilyon/backend/model/Test.java
@@ -3,14 +3,17 @@ package com.ipilyon.backend.model;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @Entity
+@AllArgsConstructor
 public class Test {
 
 	@Id
 	private Integer id;
 
 	private String test;
+
 }
