7f8dcb24f23e5a458623f2a0f984b767e48d4301
OLEWSKI Clement <clement.olewski@ipilyon.net>
2020-03-16 15:20:07 +0100

    Création d'un composant creation cours
    Signed-off-by: OLEWSKI Clement <clement.olewski@ipilyon.net>

--- frontend/src/app/modules/test/test.component.ts
+++ frontend/src/app/modules/test/test.component.ts
@@ -1,6 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {CoursDto, UlearnService} from '../../../remote';
+import {CoursDto} from '../../../remote';
 
 @Component({
     selector: 'app-test',
@@ -9,55 +8,15 @@ import {CoursDto, UlearnService} from '../../../remote';
 })
 export class TestComponent implements OnInit {
 
-    formGroup: FormGroup;
+
 
     cours: CoursDto[];
 
-    texte: string;
 
-    constructor(public formBuilder: FormBuilder,
-                private uLEARNservice: UlearnService) {
-        this.formGroup = this.formBuilder.group({
-            content: [null, Validators.required]
-        });
-    }
-
-    /**
-     * Permet de sauvegarder le contenu de l'éditeur
-     */
-    save() {
-        console.log('Sauvegarde de ' + this.formGroup.get('content').value);
+    constructor() {
     }
 
     ngOnInit() {
     }
 
-
-    //Fait une requete Http Post avec un cours dans le body
-    postCours() {
-        this.uLEARNservice.saveUsingPOST({image: null, progressions: null, texte: this.texte, titre: 'titre', video: null})
-            .subscribe(value => console.log(value), error => console.log(error), () => console.log('complete'));
-    }
-
-
-    //Fait une requete HttpGet qui renvoie une liste de Cours
-
-    getAll() {
-        this.uLEARNservice.getAllUsingGET().subscribe(
-            value => this.cours = value,
-            error => console.error(error),
-            () => {
-
-                //Insertion dans la page des cours reçus
-                let doc = this.cours.map(value => document.getElementsByClassName('cours')[0]
-                    .insertAdjacentHTML('beforeend', value.texte));
-
-                console.log('Récupération terminé');
-            }
-        );
-    }
-
-    contentChanged(value) {
-        this.texte = value;
-    }
 }
