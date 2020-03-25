ebd6e7f10109a89fb8c64459772e991734d7f52e
bertrand.cerot <bertrand.cerot@cgi.com>
2020-03-25 15:34:58 +0100

    Afficher contenu des cours en fonction des choix de l'utilisateur : OK
      A faire : Construire dynamiquement le contenu du menu (via la BDD)

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -5,11 +5,14 @@ import {BrowserModule} from "@angular/platform-browser";
 import {FormsModule} from "@angular/forms";
 import {BricolageComponent} from "./modules/bricolage/bricolage.component";
 import {LanguesComponent} from "./modules/langues/langues.component";
+import {ListeCoursComponent} from "./modules/liste-cours/liste-cours.component";
 
 export const appRouteList: Routes = [
-    { path: 'accueil', component: AccueilComponent },
-    { path: 'bricolage', component: BricolageComponent },
-    { path: 'langues', component: LanguesComponent },
+    // { path: 'accueil', component: AccueilComponent },
+    // { path: 'bricolage/:id', component: BricolageComponent },
+    // { path: 'langues', component: LanguesComponent },
+    { path: 'liste_cours/:toto', component: ListeCoursComponent},
+  // 'toto' doit correspondre au terme passé en paramètre  du params.get('toto') du liste-cours.component.ts
     { path: '**', redirectTo: 'accueil' }  // Si aucun lien trouvé ci-dessus alors redirection vers l'accueil.
 ];
 
--- frontend/src/app/modules/liste-cours/liste-cours.component.ts
+++ frontend/src/app/modules/liste-cours/liste-cours.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-liste-cours',
@@ -7,9 +8,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListeCoursComponent implements OnInit {
 
-  constructor() { }
+  constructor( private Activatedroute:ActivatedRoute ) {
 
-  ngOnInit() {
   }
 
+  maListeDeCoursBricolage: Array<{id: number, libelle: string}> = [
+    {id: 1, libelle: 'Peinture'},
+    {id: 2, libelle: 'Maconnerie'},
+    {id: 3, libelle: 'Menuiserie'},
+  ];
+
+  maListeDeCoursAAfficher: Array<{id: number, libelle: string}>;
+
+  maListeDeCoursLangues: Array<{id: number, libelle: string}> = [
+    {id: 1, libelle: 'Anglais'},
+    {id: 2, libelle: 'Français'},
+    {id: 3, libelle: 'Italien'},
+  ];
+
+  activiteA = 'a';
+  activiteB = 'b';
+  activiteC = 'c';
+
+  ngOnInit() {
+
+
+    this.Activatedroute.paramMap.subscribe(params => {
+      let libelle = params.get('toto'); // recupere moi dans ta liste des parametres que possede ActivatedRoute le parametre qui s'appelle 'libelle'
+      console.log('toto vaut : ' + libelle);
+      console.log(params);
+
+      if (libelle === 'Bricolage'){
+        this.maListeDeCoursAAfficher = this.maListeDeCoursBricolage;
+      }
+
+      if (libelle === 'Langues'){
+        this.maListeDeCoursAAfficher = this.maListeDeCoursLangues;
+      }
+
+
+  });
+
 }
+
+}
+
