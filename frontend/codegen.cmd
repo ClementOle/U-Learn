cdac5dd5a079c283b0ea9768869103e4e7b53899
bertrand.cerot <bertrand.cerot@cgi.com>
2020-08-25 12:23:39 +0200

    Ajout images

--- frontend/codegen.cmd
+++ frontend/codegen.cmd
@@ -1,2 +1,2 @@
-java -jar swagger-codegen-cli-2.3.1.jar generate -i http://localhost:8080/v2/api-docs -l typescript-angular -o src/remote --config config.json -D supportsES6=true
+"C:\Program Files\Java\jdk1.8.0_201\bin\java.exe" -jar swagger-codegen-cli-2.3.1.jar generate -i http://localhost:8080/v2/api-docs -l typescript-angular -o src/remote --config config.json -D supportsES6=true
 REM --additional-properties npmName=@techschard/language-api,snapshot=true,ngVersion=7.2.0
