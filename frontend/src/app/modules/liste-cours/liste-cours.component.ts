56ec528a9ecd7a7c125fc240885cf2fb1bfff0e5
Clément Olewski <clement.olewski@gmail.com>
2021-10-15 23:23:11 +0200

    Déplacement partie ajout commentaire
    Signed-off-by: Clément Olewski <clement.olewski@gmail.com>

--- frontend/src/app/modules/liste-cours/liste-cours.component.ts
+++ frontend/src/app/modules/liste-cours/liste-cours.component.ts
@@ -1,8 +1,6 @@
 import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute, Router} from '@angular/router';
-import {CommentaireDto, CoursDto, UlearnService, UserDto} from '../../../remote';
-import {FormBuilder, FormGroup} from '@angular/forms';
-import {AuthService} from '../../services/auth.service';
+import {ActivatedRoute} from '@angular/router';
+import {CoursDto, UlearnService} from '../../../remote';
 
 @Component({
     selector: 'app-liste-cours',
@@ -11,38 +9,24 @@ import {AuthService} from '../../services/auth.service';
 })
 export class ListeCoursComponent implements OnInit {
 
-    cours: CoursDto[];
-    coursParCategorieId: CoursDto[];
-    coursParDifficulteEtCategorie: CoursDto[];
-    loginForm: FormGroup;
+    get coursParCategorieId(): CoursDto[] {
+        return this.difficulteSelected == null ? this._coursParCategorieId : this._coursParCategorieId.filter(cours => this.difficulteSelected == cours.difficulte);
+    }
+
+    set coursParCategorieId(value: CoursDto[]) {
+        this._coursParCategorieId = value;
+    }
+
+    difficulteSelected: number;
+
+    private _coursParCategorieId: CoursDto[];
+
     difficulte: number;
     difficulteChoisie: boolean;
-    categorieIdParam: number;
-    categorieIdPrecedente: number;
-    test = new Array();
-    current: number;
-    affEcritureCom: boolean;
-    titreNouveauCom: string;
-    texteNouveauCom: string;
-    nouveauComSauvegarde: CommentaireDto;
 
-    valeurCours: CoursDto;
-    coursIdComAMaj: number;
-    listeCommentairesParCoursId: CommentaireDto [];
-    commentaireId: number;
-
-    coursAMettreAJour: CoursDto;
-    message0: string;
-    message1: string;
-    userConnected: UserDto;
-
-    c: CommentaireDto;
 
     constructor(private Activatedroute: ActivatedRoute,
-                private uLEARNservice: UlearnService,
-                private formBuilder: FormBuilder,
-                private authService: AuthService,
-                private route: Router) {
+                private uLEARNservice: UlearnService) {
 
     }
 
@@ -52,149 +36,15 @@ export class ListeCoursComponent implements OnInit {
             this.uLEARNservice.getAllCoursByCategorieIdUsingGET(+categorieId).subscribe(value => {     // Astuce : utiliser "+" pour parser un String en Int
                 this.coursParCategorieId = value;
                 this.difficulte = this.coursParCategorieId[0].difficulte;
-                console.log('coursParCategorieId vaut : ');
-                console.log(this.coursParCategorieId);
             });
         });
 
-        this.message0 = 'Afficher les commentaires';
-        this.message1 = 'Masquer les commentaires';
-
-        this.loginForm = this.formBuilder.group({
-            texteNouveauCom: [],
-            titreNouveauCom: []
-        });
         this.difficulteChoisie = false;
 
-        // Recherche de l'utilisateur connecté
-        this.uLEARNservice.getUserByUserNameUsingGET(this.authService.userNameConnected).subscribe(value => {
-            this.userConnected = value;
-        });
     }
 
-    // Mise à jour du libellé du bouton commentaire et affichage ou non des commentaires
-    // Mise à jour du booléen d'affichage de commentaires en BDD
-    majCom(coursAUpdate) {
-        this.coursAMettreAJour = coursAUpdate;
-
-        if (this.coursAMettreAJour.afficheCommentaires == 1) {
-            this.coursAMettreAJour.afficheCommentaires = 0;
-        } else {
-            this.coursAMettreAJour.afficheCommentaires = 1;
-        }
-        this.uLEARNservice.putCoursByCoursIdUsingPUT(this.coursAMettreAJour).subscribe(value => {
-                this.coursAMettreAJour = value;
-            }
-        );
+    choixDifficulteEtCategorie(difficulte: number) {
+        this.difficulteSelected = difficulte;
     }
-
-
-    choixDifficulteEtCategorie(difficulte) {
-        this.Activatedroute.paramMap.subscribe(params => {
-                let categorieIdParam = params.get('paramChemin');
-                this.categorieIdParam = (+categorieIdParam);
-
-                // Controle si on clique dans la navBar sur une autre catégorie que la catégorie actuelle.
-                // Si oui on affiche tous les cours de la nouvelle catégorie choisie.
-                if (this.categorieIdParam != this.categorieIdPrecedente) {
-                    this.difficulteChoisie = false;
-                }
-            }
-        );
-        this.coursParDifficulteEtCategorie = null;
-        this.uLEARNservice.getAllCoursByDifficulteAndCategorieUsingGET(this.categorieIdParam, difficulte).subscribe(value => {
-                this.coursParDifficulteEtCategorie = value;
-                console.log('this.coursParDifficulteEtCategorie');
-                console.log(this.coursParDifficulteEtCategorie);
-                this.difficulteChoisie = true;
-                difficulte = null;
-                this.categorieIdPrecedente = this.categorieIdParam;
-            }
-        );
-    }
-
-  savePost(nouveauCommentaire) {
-    // Sauvegarde du commentaire
-    this.uLEARNservice.saveCommentsUsingPOST(nouveauCommentaire).subscribe( value => {
-        console.log('lance this.saveCommentsUsingPOST()');
-        console.log('value');
-        console.log(value);
-
-        this.nouveauComSauvegarde = value;
-        this.commentaireId = this.nouveauComSauvegarde.commentaireId
-
-        this.afficheComApresSave();
-      }
-
-    );
-  }
-
-  afficheComApresSave() {
-    for (let i = 0; i < this.coursParDifficulteEtCategorie.length; i++) {
-      if (this.coursParDifficulteEtCategorie[i].coursId == this.coursIdComAMaj) {
-
-        this.coursParDifficulteEtCategorie[i].commentaires.push(this.nouveauComSauvegarde);
-      }
-    }
-  }
-
-  onValiderCom(cours) {
-    console.log('lance this.onValiderCom()');
-
-      const nouveauCommentaire: CommentaireDto = {
-        commentaireId: 0,
-        user: this.userConnected,
-        cours: cours,
-        titreCommentaire: this.loginForm.value.titreNouveauCom,
-        texteCommentaire: this.loginForm.value.texteNouveauCom,
-        commentaireUtile: '10',
-        afficheBooleen: true
-        };
-
-      this.valeurCours = cours.valueOf();
-      this.coursIdComAMaj = this.valeurCours.coursId;
-
-      this.savePost(nouveauCommentaire);
-      // // Sauvegarde du commentaire
-      // this.uLEARNservice.saveCommentsUsingPOST(nouveauCommentaire).subscribe( value => {
-      //   console.log('lance this.saveCommentsUsingPOST()');
-      //   console.log('value');
-      //   console.log(value);
-      //
-      //   this.nouveauComSauvegarde = value;
-      //   this.commentaireId = this.nouveauComSauvegarde.commentaireId
-      //   }
-      //
-      // );
-      this.affEcritureCom = false;
-      // this.afficheComApresSave();
-
-      // for (let i = 0; i < this.coursParDifficulteEtCategorie.length; i++) {
-      //   if (this.coursParDifficulteEtCategorie[i].coursId == this.coursIdComAMaj) {
-      //     console.clear();
-      //     console.log('this.coursParDifficulteEtCategorie[i].commentaires vaut ');
-      //     console.log(this.coursParDifficulteEtCategorie[i].commentaires[this.commentaireId].titreCommentaire);
-      //     console.log(this.coursParDifficulteEtCategorie[i].commentaires[this.commentaireId].texteCommentaire);
-      //   }
-      // }
-
-      // this.uLEARNservice.getAllCommentairesByCoursIdUsingGET(this.coursIdComAMaj).subscribe(value => {
-      //   this.listeCommentairesParCoursId = value;
-      //   console.log('this.listeCommentairesParCoursId');
-      //   console.log(this.listeCommentairesParCoursId);
-      // });
-
-      this.majCom(cours);
-
-    }
-
-    ecrireCom() {
-        this.affEcritureCom = true;
-    }
-
-    onAnnulerCom() {
-        this.affEcritureCom = false;
-    }
-
 
 }
